0664c6b54b962a8bf898533acbe3bdb098d6d802
Amarnath  Karthi <97amarnathk@gmail.com>
2021-02-21 14:26:52 +0530

    remove cartoon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import cartoon from "../assets/cartoon.png";
 import { HeroSection } from "../components/heroSection";
 import { RecentPosts } from "../components/recentPosts";
 import { DefaultLayout } from "../layouts/defaultLayout";
 
 export default function Home() {
-  { console.log(cartoon) }
   return (
     <DefaultLayout>
       <div class="space-y-16">
