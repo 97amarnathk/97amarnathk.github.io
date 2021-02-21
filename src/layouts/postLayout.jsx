0b8d053b0b3bac306d8291924c74b9b3397cd297
Amarnath  Karthi <97amarnathk@gmail.com>
2021-02-21 19:32:37 +0530

    add mdx styling

--- src/layouts/postLayout.jsx
+++ src/layouts/postLayout.jsx
@@ -1,18 +1,25 @@
 import { format } from 'date-fns';
+import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import React from "react";
 import { DefaultLayout } from "../layouts/defaultLayout";
 
 export default ({ data }) => {
   const { frontmatter, body } = data.mdx;
+  const date = format(new Date(frontmatter.date), "MMM dd, yyyy")
   return (
     <DefaultLayout>
       <article class="mx-auto max-w-5xl">
         <header class="mb-12">
           <h1 class="my-2 text-4xl font-semibold">{frontmatter.title}</h1>
-          <p>{format(new Date(frontmatter.date), "MMM dd, yyyy")}</p>
+          <div class="flex space-x-16">
+            <p class="text-base text-gray-600 font-mono">{date}</p>
+            <p class="text-base text-gray-600 font-mono">Tools</p>
+          </div>
         </header>
-        <MDXRenderer>{body}</MDXRenderer>
+        <div class="markdown">
+          <MDXRenderer>{body}</MDXRenderer>
+        </div>
       </article>
 
     </DefaultLayout >
